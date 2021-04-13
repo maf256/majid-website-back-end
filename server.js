1b2cf5453f4520561b6593a1f079d6c2300320de
Majid Rahimi <Majid88Rahimi@gmail.com>
2021-04-13 12:59:06 +0300

    Added GET form/save API endPoint

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const mysql      = require('mysql');
 const connection = mysql.createConnection({
     host     : '212.16.187.118',
@@ -8,12 +7,13 @@ const connection = mysql.createConnection({
     password : '4hwJt!A9jLVTKCa',
     database : 'toyenblo_majid-website'
 });
+const cors = require('cors')
 
-
+app.use(cors())
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-app.post('/', (req, res) => {
+app.post('/form/save', (req, res) => {
     const formData = req.body;
     const sql = `
         INSERT INTO
@@ -22,14 +22,30 @@ app.post('/', (req, res) => {
             ?
     `
     connection.query(sql, formData, function (error, results, fields) {
-        if (error) throw error;
-        // connected!
-        console.log('results', results);
-        console.log('fields', fields);
+        if (error) {
+            res.status(500).jsonp({ error: 'Somthing went wrong!' })
+        } else {
+            res.status(200).jsonp({ msg: 'Everything went well!' })
+        }
     });
-
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.get('/form/save', (req, res) => {
+    const sql = `
+        SELECT
+            *
+        FROM
+            form
+    `
+    connection.query(sql, function (error, results, fields) {
+        if (error) {
+            res.status(500).jsonp({ error: 'Somthing went wrong!' })
+        } else {
+            res.status(200).jsonp(results)
+        }
+    });
+})
+
+app.listen(3000, () => {
+  console.log(`Example app listening at http://localhost:3000`)
 })
